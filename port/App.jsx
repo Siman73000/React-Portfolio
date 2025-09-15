e72f996efc0117770dd76a3e4737f4ca0eb797be
Siman73000 <si.p.h@outlook.com>
2025-09-15 09:30:48 -0500

    rendering

--- port/App.jsx
+++ port/App.jsx
@@ -294,6 +294,9 @@ export default function App() {
         </div>
       </motion.section>
 
+      <Experience />
+      
+
       {/* Contact Section */}
       <motion.section
         id="contact"
