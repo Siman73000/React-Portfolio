98cd225f7ed9a43614e8d6fe488fd569fb6882b6
Siman73000 <si.p.h@outlook.com>
2025-09-16 13:14:03 -0500

    hook revision

--- port/useIsMobile.jsx
+++ port/useIsMobile.jsx
@@ -1,13 +1,12 @@
-export default function useIsMobile(breakpoint = 768) {
+// Hook matches the CSS mobile breakpoint
+export default function useIsMobile(breakpoint = 600) {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
 
-    // Set initial value
     setIsMobile(mediaQuery.matches);
 
-    // Listener for changes
     const handler = (event) => setIsMobile(event.matches);
     mediaQuery.addEventListener("change", handler);
 
