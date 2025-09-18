8d2a5f60b6e0ad9bed3c4de39cc7ae1c17004959
Siman73000 <si.p.h@outlook.com>
2025-09-18 14:35:23 -0500

    removed initial hook

--- port/App.jsx
+++ port/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import heroImage from "/adrien-olichon-RCAhiGJsUUE-unsplash.jpg";
 import "./App.css";
-import useIsMobile from "./useIsMobile";
 
 export function Experience({ experience }) {
   return (
